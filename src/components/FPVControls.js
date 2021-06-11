fef67b8fe249e5ff2c0ded9031ee000d5c000126
Chethas K <57009942+Chethas47@users.noreply.github.com>
2021-06-11 11:28:08 +0530

    First Commit

---
+++ src/components/FPVControls.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { PointerLockControls as PointerLockControlsImpl } from 'three/examples/jsm/controls/PointerLockControls';
+import { useThree, extend } from 'react-three-fiber';
+
+extend({ PointerLockControlsImpl });
+
+export const FPVControls = (props) => {
+  const { camera, gl } = useThree();
+  const controls = useRef();
+
+  useEffect(() => {
+    document.addEventListener('click', () => {
+      controls.current.lock();
+    });
+  }, []);
+
+  return (
+    <pointerLockControlsImpl
+      ref={controls}
+      args={[camera, gl.domElement]}
+      {...props}
+    />
+  );
+};
